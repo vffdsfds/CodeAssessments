1fea5641795e382bf1ab92ceb7c314f4b861f2b7
vffdsfds <1582151629@qq.com>
2019-04-29 18:36:57 -0400

    new

---
+++ CodeAssessments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeAssessments
+//
+//  Created by Xin Chen on 2019/4/29.
+//  Copyright © 2019年 Xin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
